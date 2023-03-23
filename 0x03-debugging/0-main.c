867d454dd96a648c3eed85acde236c1e2c6e0af2
amoskarugo <karugoamos@gmail.com>
2023-03-23 10:19:04 +0000

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include <time.h>
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+	printf("%i is zero\n", n);
+	}
+	else if (n > 0)
+	{
+	printf("%i is positive\n", n);
+	}
+	else
+	{
+	printf("%i is negative\n", n);
+	}
+	return (0);
+}
