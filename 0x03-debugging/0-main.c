4707fdbf343303574dd791ec84aeddd65293137d
amoskarugo <karugoamos@gmail.com>
2023-03-23 10:31:02 +0000

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 #include <time.h>
 /**
 * main - print if the number is postive, zero, or negative
-*
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
@@ -12,8 +11,6 @@ int main(void)
 	int i;
 
 	i = 60 % 2;
-
 	positive_or_negative(i);
-
 	return (0);
 }
