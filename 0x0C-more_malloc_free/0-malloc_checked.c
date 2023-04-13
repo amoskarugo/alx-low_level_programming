a5c09755e09d9bc47e69f6d3bf3b4a651e313e2b
amoskarugo <karugoamos@gmail.com>
2023-04-13 19:04:34 +0000

    #

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+*malloc_checked - function entry point
+*@b: amount to be allocated
+*Return: pointer to new memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *n
+
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
+
