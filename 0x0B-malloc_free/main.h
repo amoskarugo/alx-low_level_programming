166939137577dc20fd994ce352ca7a130b13ea79
amoskarugo <karugoamos@gmail.com>
2023-04-13 12:32:45 +0000

    a function that creates array of chars and initializes it with a specific char

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(char c)
-char *create_array(unsigned int size, char c)
+void _putchar(char c);
+char *create_array(unsigned int size, char c);
 #endif
