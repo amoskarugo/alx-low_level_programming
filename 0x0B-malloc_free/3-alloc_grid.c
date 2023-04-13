688b48e83fd7f8e0baa5b95dbe63ca21cfdc53e4
amoskarugo <karugoamos@gmail.com>
2023-04-13 18:15:07 +0000

    #

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 /**
  * alloc_grid - a function that returns a pointer to
@@ -8,36 +7,35 @@
  * @width: width
  * @height: height
  *
- * Return: NULL on faliure
+ * Return: returns the array or NULL on failure
 */
 
 int **alloc_grid(int width, int height)
 {
-int **arr;
-int i, j;
+	int **arr, i, j;
 
-if (width < 1)
-	return (NULL);
-if (height < 1)
-	return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-arr = (int **)malloc(sizeof(int *) * height);
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
 
-if (arr == NULL)
-	return (NULL);
-
-for (i = 0; i < height; i++)
-{
-	arr[i] = (int *)malloc(width * sizeof(int));
-	for (j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
 	{
-	arr[i][j] = 0;
-	free(arr[i]);
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(arr[i]);
+				i--;
+			}
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
 	}
-}
-	free(arr);
-
 	return (arr);
-
 }
-
