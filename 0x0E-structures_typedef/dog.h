e30ce820b88f38f579f537025e074e748b935e0a
amoskarugo <karugoamos@gmail.com>
2023-04-17 18:08:42 +0000

    #

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+*struct dog - a structure to create a dog abject
+*@name: name of the dog
+*@owner: owner of the dog
+*@age: age of the dog
+*/
 struct dog
 {
 	char *name;
