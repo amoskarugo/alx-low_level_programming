12e366ed91cc9f0f8fb0cd32ff65652dded61ae6
amoskarugo <karugoamos@gmail.com>
2023-04-28 21:28:38 +0000

    #

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,8 @@
-global _start
+global main
 
 section .text
 
-_start:
+main:
   mov rax, 1
   mov rdi, 1
   mov rsi, msg
@@ -14,5 +14,5 @@ _start:
   syscall
 
 section .rodata
-  msg: db "Hello, Holberton\n", 10
+  msg: db "Hello, Holberton", 10
   msglen: equ $ - msg
