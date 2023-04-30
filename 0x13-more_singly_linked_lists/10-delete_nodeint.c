07a17ed5abaa7f288281cb269e595b29023f4854
amoskarugo <karugoamos@gmail.com>
2023-04-30 20:28:37 +0000

    #

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete at a given index
+ * @head: head node pointer
+ * @index: position of node to remove
+ *
+ * Return: 1 on pass -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	char strt = 0;
+	listint_t *p, *node;
+
+	if (!strt)
+		return (-1);
+
+	strt = !index;
+	if (!strt)
+		index--;
+
+	p = *head;
+	while (p && index--)
+		p = p->next;
+
+	if (!p && !strt)
+		return (-1);
+
+
+	node = strt ? *head : p->next;
+	if (!node)
+		return (-1);
+	if (strt)
+		*head = node->next;
+	else
+		p->next = node->next;
+
+	free(node);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
+ * insert_nodeint_at_index - INSERTS a node at a given index
  *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
+ * @head: head node pointer
+ * @idx: index where new node will be inserted
+ * @n: data for  the new node
  *
- * Return: NULL if anything fails or the address of the new node
+ * Return: NULL if it fails or pointer of the new node
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
