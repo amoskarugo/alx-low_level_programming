b384e874860fd3f6df10d8981de68dd77d3bceb7
amoskarugo <karugoamos@gmail.com>
2023-04-30 19:21:14 +0000

    #

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - removes the head of the node
+ *
+ * @head: head node pointer
+ *
+ * Return: the head node data
+*/
+int pop_listint(listint_t **head)
+{
+    int node1_data;
+    listint_t *makeshift, *next;
+
+    if (*head == NULL)
+        return (0);
+
+    makeshift = *head;
+    next = makeshift->next;
+    node1_data = temp->n;
+
+    free(makeshift);
+    *head = next;
+
+    return (node1_data);
+}
