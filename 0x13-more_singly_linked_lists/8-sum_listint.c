2036645b211330c97c5a797d7d6d01e174a2f902
amoskarugo <karugoamos@gmail.com>
2023-04-30 19:51:08 +0000

    sum of all elements in a list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,13 +5,13 @@
  *
  * @head: head node pointer
  *
- * Return: sum 
+ * Return: sum
 */
 int sum_listint(listint_t *head)
 {
 	if (head == NULL)
-		return 0;
+		return (0);
 	else
-		return head->n + sum_listint(head->next);
+		return (head->n + sum_listint(head->next));
 }
 
