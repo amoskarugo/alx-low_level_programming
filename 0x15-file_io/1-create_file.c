011ced75c64a35fb36b6566d73f53ae271dc9177
amoskarugo <karugoamos@gmail.com>
2023-05-08 23:33:19 +0000

    #

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,55 +1,27 @@
 #include "main.h"
+
 /**
- * create_file - creates a file.
- * @filename: name of the file
- * @text_content: content to be wrritten in the file
- * Return: 1 on succes, -1 whent it fails
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+
+int	create_file(const char *filename, char *text_content)
 {
-	int d, ans;
-	size_t len = 0;
-	mode_t md = S_IRUSR | S_IWUSR;
+	int	fd;
+	int	w;
 
-	if (!filename)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
-	if (text_content)
-	{
-		while (text_content[len])
-			len++;
-	}
-	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, md);
-	if (d == -1)
-	{
-		perror("open");
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	}
-	if (len > 0)
-	{
-		char *buffer = malloc(len * sizeof(char));
-
-		if (!buffer)
-		{
-			perror("malloc");
-			close(d);
-			return (-1);
-		}
-		memcpy(buffer, text_content, len);
-		ans = write(d, buffer, len);
-		free(buffer);
-	}
-	else
-	{
-		ans = 1;
-	}
-	if (ans == -1)
-	{
-		perror("write");
-		close(d);
+	if (text_content == NULL)
+		text_content = "";
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
 		return (-1);
-	}
-	close(d);
+	close(fd);
 	return (1);
 }
