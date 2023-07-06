ef8c12f50850c343dbf16490e452dcd024a1becf
amoskarugo <karugoamos@gmail.com>
2023-07-06 16:28:07 +0000

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,17 @@
  *
  * Return: value of the bit, or -1 if there is an error
  */
+
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	unsigned long int div, check;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
+	div = 1 << index;
+	check = n & div;
+	if (check == div)
 		return (1);
 	return (0);
 }
