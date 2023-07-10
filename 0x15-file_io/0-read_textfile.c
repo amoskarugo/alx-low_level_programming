bb704ee7b2ccf709e47628b5e29238e263c09eda
amoskarugo <karugoamos@gmail.com>
2023-07-10 09:53:50 +0000

    #

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,37 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it.
- * @filename: yes, it's a file name
- * @letters: num of letters
- * Return: num of letters
- * on error 0 returned
+ * read_textfile - read text and print it to POSIX
+ * @filename: name of file char
+ * @letters:number of letters to read size_t
+ * Return: read the file display it or error 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t num_of_letters;
-	ssize_t bytes_read;
-	int fd;
-	char *buff;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (0);
-	buff = malloc(letters);
-	if (!buff)
-		return (0);
-	fd = open(filename, O_RDONLY);
+	}
+
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
+	{
 		return (0);
-	bytes_read = read(fd, buff, letters);
-	num_of_letters = write(STDOUT_FILENO, buff, bytes_read);
-	free(buff);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
 	close(fd);
-	return (num_of_letters);
+	free(buff);
+	return (wd);
 }
