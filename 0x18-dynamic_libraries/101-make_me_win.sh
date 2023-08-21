1dfcae7397dd22aa3cb99ef929525632a1efda78
amoskarugo <karugoamos@gmail.com>
2023-08-21 21:21:09 +0000

    #

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/amoskarugo/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libwinner.so
+wget -P /tmp https://github.com/amoskarugo/alx-low_level_programming/0x17-dynamic_libraries/libwinner.so
 export LD_PRELOAD=/tmp/libwinner.so
