4b70644bc628426344221a65710662c4bff5676d
amoskarugo <karugoamos@gmail.com>
2023-08-21 18:02:00 +0000

    dynamic library

---
+++ 0x18-dynamic_libraries/c_files/lib3.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	return (src);
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return (src);
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (src);
+}
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/c_files/lib4.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (src);
+}
+char *_strchr(char *s, char c)
+{
+	return (s);
+}
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle);
+}
